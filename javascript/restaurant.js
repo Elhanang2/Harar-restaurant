5b78779013a2025b07280760a2aea2fb87cacda7
Ayna <aynazewd@yahoo.com>
2019-12-18 13:11:06 -0800

    styling and some javascript for navbar

--- javascript/restaurant.js
+++ javascript/restaurant.js
@@ -11,9 +11,10 @@ $(document).ready(function() {
   });
   // When the user scrolls the page, execute myFunction
   window.onscroll = function() {
-    myFunction();
+    navbarSticky();
+    
   };
-
+  
   // Get the navbar
   var navbar = document.getElementById("navigation-bar");
 
@@ -21,17 +22,18 @@ $(document).ready(function() {
   var sticky = navbar.offsetTop;
 
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function myFunction() {
+  function navbarSticky() {
     if (window.pageYOffset >= sticky) {
       navbar.classList.add("sticky");
     } else {
       navbar.classList.remove("sticky");
     }
   }
-  //Dish image automatic slide show
 
+
+  //Dish image automatic slide show
   var slideIndex = 0;
-  showSlides();
+    showSlides();
 
   function showSlides() {
     var slides = $(".mySlides");
@@ -51,32 +53,100 @@ $(document).ready(function() {
     
     setTimeout(showSlides, 2000);
   }
+
+
   $(".breakfast").on("click", function (e) {
     e.preventDefault();
-    $.get("pages/breakfast.html", function (data) {
+    $.get("./pages/breakfast.html", function (data) {
         $("#eachMenu-page").html(data);
         $(".front h1").text("breakfast menu");
-
-    });
-    
+        $(".front,.back").css("background-color","orange"); 
+    }); 
   });
+
+
   $(".lunch").on("click", function (e) {
     e.preventDefault();
-    $.get("pages/lunch.html", function (data) {
+    $.get("./pages/lunch.html", function (data) {
         $("#eachMenu-page").html(data);
         $(".front h1").text("lunch menu");
+        $(".front,.back").css("background-color"," rgb(11, 155, 199)");
+        
     });
     
   });
+
 
   $(".dinner").on("click", function (e) {
     e.preventDefault();
     $.get("./pages/dinner.html", function (data) {
         $("#eachMenu-page").html(data);
         $(".front h1").text("dinner menu");
-    });
-    
+        $(".front,.back").css("background-color","navy");  
+    }); 
   });
+
+ 
+//  Check a href for an anchor. If exists, and in document, scroll to it.
+function scroll_if_anchor(href) {
+  href = typeof(href) == "string" ? href : $(this).attr("href");
+  
+  // You could easily calculate this dynamically if you prefer
+  var fromTop = 80;
+  // If our Href points to a valid, non-empty anchor, and is on the same page
+  if(href.indexOf("#") == 0) {
+      var $target = $(href);
+      if($target.length) {
+          $('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+          if(history && "pushState" in history) {
+              history.pushState({}, document.title, window.location.pathname + href);
+              return false;
+          }
+      }
+  }
+}    
+
+// When our page loads, check to see if it contains and anchor
+scroll_if_anchor(window.location.hash);
+
+// Intercept all anchor clicks
+$("body").on("click", "a", scroll_if_anchor);
+const numofnavlist = document.getElementsByClassName("navlist");
+
+
+for(let i=0; i< numofnavlist.length; i++){
+  numofnavlist[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  })
+}
+
+var sections = $('section')
+  , nav = $('#navigation-bar')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+        if(cur_pos == 0){
+          top = $(this).offset().top
+        }
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
 });
 
   
